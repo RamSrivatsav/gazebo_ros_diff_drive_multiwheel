01245920c8f6672e9b42cbee9cfb7e12974a6a0b
RamSrivatsav <vatsav.ben@gmail.com>
2020-12-16 16:07:12 +1100

    cleaned the plugin code

--- src/gazebo_ros_diff_drive_multiwheel.cpp
+++ src/gazebo_ros_diff_drive_multiwheel.cpp
@@ -342,31 +342,12 @@ void GazeboRosDiffDriveMW::UpdateChild()
             if ( wheel_accel == 0 ||
                     ( fabs ( wheel_speed_[LEFT] - current_speed[LEFT] ) < 0.01 ) ||
                     ( fabs ( wheel_speed_[RIGHT] - current_speed[RIGHT] ) < 0.01 ) ) {
-                //if max_accel == 0, or target speed is reached
-                // joints_[LEFT]->SetParam ( "vel", 0, wheel_speed_[LEFT]/ ( wheel_diameter_ / 2.0 ) );
-                // joints_[RIGHT]->SetParam ( "vel", 0, wheel_speed_[RIGHT]/ ( wheel_diameter_ / 2.0 ) );
                 joints_[side][i]->SetParam ( "vel", 0, wheel_speed_[side]/ ( wheel_diameter_ / 2.0 ) );
             } else {
-                // if ( wheel_speed_[LEFT]>=current_speed[LEFT] )
-                //     wheel_speed_instr_[LEFT]+=fmin ( wheel_speed_[LEFT]-current_speed[LEFT],  wheel_accel * seconds_since_last_update );
-                // else
-                //     wheel_speed_instr_[LEFT]+=fmax ( wheel_speed_[LEFT]-current_speed[LEFT], -wheel_accel * seconds_since_last_update );
-
-                // if ( wheel_speed_[RIGHT]>current_speed[RIGHT] )
-                //     wheel_speed_instr_[RIGHT]+=fmin ( wheel_speed_[RIGHT]-current_speed[RIGHT], wheel_accel * seconds_since_last_update );
-                // else
-                //     wheel_speed_instr_[RIGHT]+=fmax ( wheel_speed_[RIGHT]-current_speed[RIGHT], -wheel_accel * seconds_since_last_update );
-
                 if ( wheel_speed_[side]>current_speed[side] )
                     wheel_speed_instr_[side]+=fmin ( wheel_speed_[side]-current_speed[side], wheel_accel * seconds_since_last_update );
                 else
                     wheel_speed_instr_[side]+=fmax ( wheel_speed_[side]-current_speed[side], -wheel_accel * seconds_since_last_update );
-
-                // ROS_INFO_NAMED("diff_drive", "actual wheel speed = %lf, issued wheel speed= %lf", current_speed[LEFT], wheel_speed_[LEFT]);
-                // ROS_INFO_NAMED("diff_drive", "actual wheel speed = %lf, issued wheel speed= %lf", current_speed[RIGHT],wheel_speed_[RIGHT]);
-
-                // joints_[LEFT]->SetParam ( "vel", 0, wheel_speed_instr_[LEFT] / ( wheel_diameter_ / 2.0 ) );
-                // joints_[RIGHT]->SetParam ( "vel", 0, wheel_speed_instr_[RIGHT] / ( wheel_diameter_ / 2.0 ) );
                 joints_[side][i]->SetParam ( "vel", 0, wheel_speed_instr_[side] / ( wheel_diameter_ / 2.0 ) );
             }
           }
